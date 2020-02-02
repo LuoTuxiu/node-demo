30894d6d05c0da9888edb6be86e0969c2dbd5a30
Luotuxiu <359703836@qq.com>
2020-02-02 23:09:27 +0800

    feat: init project

---
+++ index.js
@@ -0,0 +1,34 @@
+const Koa = require('Koa')
+const app = new Koa()
+
+app.use(async (ctx, next) => {
+  await next()
+  const rt = ctx.response.get('X-Response-Time')
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+})
+
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  ctx.set('X-Response-Time', `${ms}ms`)
+})
+
+
+app.use(async ctx => {
+  console.log(ctx)
+  ctx.body = 'hello xiuxiu'
+  console.log(ctx.body);
+  ctx.set({ 'Access-Control-Allow-Origin': "*" })
+  if (ctx.originalUrl === '/user/login') {
+    ctx.body = {
+      status: 'success',
+      code: 200,
+      data: {
+        name: 'xiuxiu'
+      }
+    }
+  }
+})
+
+app.listen(3001)
